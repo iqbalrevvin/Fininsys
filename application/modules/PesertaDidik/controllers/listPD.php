025f1c52fcd195a4261597ff4ad05970c74c2f95
iqbalrevvin <iqbal.revvin@gmail.com>
2019-04-20 18:21:59 +0700

    Upgrade To HMVC

---
+++ application/modules/PesertaDidik/controllers/listPD.php
@@ -0,0 +1,73 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Listpd extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('outputView');
+		$this->load->library('grocery_CRUD');
+		$this->load->model('PesertaDidik_m');
+	}
+
+	public function index(){
+		$crud 		= new grocery_CRUD();
+
+		$crud->set_table('peserta_didik');
+		$crud->set_subject('Data Utama Peserta Didik');
+
+		$crud->set_field_upload('foto_pd', 'assets/image/foto_pd');
+
+		$crud->display_as('nama_pd', 'Nama Peserta Didik');
+		$crud->display_as('NIK_pd', 'NIK Peserta Didik');
+		$crud->display_as('jk_pd', 'Jenis Kelamin');
+		$crud->display_as('tempat_lahir_pd', 'Tempat Lahir');
+		$crud->columns('NIK_pd', 'tempat_lahir_pd', 'tanggal_lahir_pd', 'nama_pd', 'jk_pd', 'no_telp_pd');
+		$crud->add_action('Profil', 'fa fa-user', '', '',array($this,'profilLink'));
+		$crud->set_rules('NIK_pd','NIK Siswa','required|numeric|max_length[16]|min_length[16]');
+		$crud->unset_read();
+
+		/*RELASI*/
+
+		/*-----------------------------------------------*/
+		$crud->unset_add_fields('kelurahan','kecamatan', 'kota', 'provinsi');
+
+		#$crud->required_fields('NIK_pd','nama_pd', 'jk_pd', 'tempat_lahir_pd', 'agama', 'no_telp_pd');
+		$crud->callback_after_insert(array($this,'insertDetail'));
+		#$crud->callback_insert(array($this,'insert'));
+		
+
+		$output = $crud->render();
+
+		$data['judul'] 		= 'Daftar Pesera Didik';
+		$template 			= 'admin_template';
+		$view 				= 'grocery';
+
+		$this->outputview->output_admin($view, $template, $data, $output);
+	}
+
+	function nama_callback($value, $primary_key = null){
+		$value = '<b>'.$value.'</b>';
+		return $value;
+	}
+
+	function insertDetail($post_array){
+		
+		$nik 	= $post_array['NIK_pd'];
+		$data = [
+		    'NIK_pd' => $nik,
+		];
+
+		$insertDetail = $this->PesertaDidik_m->insertDetail($data);
+		return $insertDetail;
+	}
+
+	function profilLink($primary_key, $row){
+		return site_url('pesertadidik/profil').'?ID='.$row->NIK_pd;
+	}
+
+}
+
+/* End of file datapd.php */
+/* Location: ./application/controllers/PesertaDidik/datapd.php */
---
+++ application/modules/PesertaDidik/views/profil.php
@@ -0,0 +1,51 @@
+<!--<div id="resultFormRegister"></div>-->
+<!-- <?php if($this->session->flashdata('sukses')): ?>
+  <div class="col-xl-12">
+    <div class="m-alert m-alert--icon m-alert--outline alert alert-success alert-dismissible fade show m-alert--air" role="alert">
+      <div class="m-alert__icon">
+        <i class="la la-warning"></i>
+      </div>
+      <div class="m-alert__text">
+        <strong>Data Terkirim!</strong> <?= $this->session->flashdata('sukses'); ?> !
+        <a href="#">Lihat Data Nasabah</a>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"></button>
+      </div>
+    </div>
+  </div>
+<?php endif; ?> -->
+
+<div id="resultProfil">
+    <div class="m-blockui" id="loader-center">
+        <span>Memuat Profil . .</span>
+        <span>
+            <div class="m-loader m-loader--brand"></div>
+        </span>
+    </div>
+</div>
+<input type="hidden" id="dataID" value="<?= $this->input->get('ID') ?>">
+<?php #require('formRegistration.php') ?>
+
+<script>
+	$(document).ready(function() {
+		formRegisterView()
+		function formRegisterView(){
+          var id = $('#dataID').val();
+          $.ajax({
+              url: '<?= base_url('PesertaDidik/Profil/getProfil') ?>',
+              type: 'POST',
+              async: true,
+              data:{
+                  ID : id,
+                  show: 1
+              },
+              success: function(response){
+                  $('#resultProfil').html(response);
+                  //$("#jenisNasabah").selectpicker();
+              }
+          });
+        } 
+
+	});
+</script>
+
+
