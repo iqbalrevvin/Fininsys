d6181802dfa3dfe9f46e36d12fb2b9ddc0733c71
iqbalrevvin <iqbal.revvin@gmail.com>
2019-05-27 05:25:51 +0700

    Update config.php
    Base Url Dinamis

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/Fininsys/';
+//$config['base_url']	= 'http://localhost/Fininsys/';
+
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 /*
 |--------------------------------------------------------------------------
 | Index File
