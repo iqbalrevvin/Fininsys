ce3829ed7e01eea786fe3c78a3ec3fbd708838ac
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-04 08:44:26 +0700

    Update fotoGender_helper.php

--- application/helpers/fotoGender_helper.php
+++ application/helpers/fotoGender_helper.php
@@ -1,4 +1,4 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php  defined('BASEPATH') OR exit('No direct script access allowed');
 
 if (! function_exists('fotoGender')){
 	function fotoGender($foto, $JK){
@@ -15,10 +15,6 @@ if (! function_exists('fotoGender')){
 	}
 }
 
-function tester()
-{
-	$test = "test";
-	return $test;
-}
+
 
 ?>
