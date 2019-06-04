00b86248912fc4751f502221e21515d0833da6a7
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-04 09:38:36 +0700

    Update tglIndo_helper.php

--- application/helpers/tglIndo_helper.php
+++ application/helpers/tglIndo_helper.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
       
-    if (!function_exists('tgl_indo')){
+    if (!function_exists('date_indo')){
         function date_indo($tgl){
             $ubah = gmdate($tgl, time()+60*60*8);
             $pecah = explode("-",$ubah);
