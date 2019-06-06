0be72c32a3c726d194bea31b3c097b8505b61c92
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-07 06:35:46 +0700

    Update ImportPesertaDidik.php

--- application/modules/ImportMaster/controllers/ImportPesertaDidik.php
+++ application/modules/ImportMaster/controllers/ImportPesertaDidik.php
@@ -78,7 +78,7 @@ class ImportPesertaDidik extends CI_Controller {
 		$jumlahGagal = $jumlahData-$dataMasuk;
 		$this->session->set_flashdata('suksesImport', '<b>'.$jumlahData. '</b> Data Diproses | <b class="text-success">'.$dataMasuk.'</b> Data Berhasil Terimport | <b class="text-danger">'.$jumlahGagal.'</b> Data Gagal Terimport');
 
-		redirect('refresh');
+		redirect($this->uri->uri_string(),'refresh');
 
 	}
 
