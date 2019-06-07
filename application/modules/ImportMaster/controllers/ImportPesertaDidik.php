5121f39237875dd6c8ad69d609ef711ee2a639c9
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-07 08:06:30 +0700

    Update ImportPesertaDidik.php

--- application/modules/ImportMaster/controllers/ImportPesertaDidik.php
+++ application/modules/ImportMaster/controllers/ImportPesertaDidik.php
@@ -72,10 +72,11 @@ class ImportPesertaDidik extends CI_Controller {
 				$this->Importmaster_m->importDataUtama($data);
 			}
 			$numrow++;
+			$numr[] = $numrow++
 			
 		}
 		
-		$jumlahData = count($data);
+		$jumlahData = count($numr);
 		$dataMasuk = $this->db->affected_rows();
 		$jumlahGagal = $jumlahData-$dataMasuk;
 		$this->session->set_flashdata('suksesImport', '<b>'.$jumlahData. '</b> Data Diproses | <b class="text-success">'.$dataMasuk.'</b> Data Berhasil Terimport | <b class="text-danger">'.$jumlahGagal.'</b> Data Gagal Terimport');
