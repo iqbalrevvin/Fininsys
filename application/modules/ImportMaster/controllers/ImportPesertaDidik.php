ec4f21a0894e985bfcce9a3d1fbb0456b8dcdb7f
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-09 02:32:31 +0700

    Update ImportPesertaDidik.php

--- application/modules/ImportMaster/controllers/ImportPesertaDidik.php
+++ application/modules/ImportMaster/controllers/ImportPesertaDidik.php
@@ -50,7 +50,7 @@ class ImportPesertaDidik extends CI_Controller {
 		#$data = array();
 		$numrow =1;
 		foreach($sheet as $row){
-			if($numrow > 2){
+			if($numrow > 1){
 				$data = array(
 					'idSekolah'			=>$row['A'], 
 					'NIK_pd'			=>$row['B'],
