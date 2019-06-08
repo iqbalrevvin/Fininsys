fc27094cc1c0e753088655d7565f677d71f8f39c
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-09 01:33:22 +0700

    Update daftarSiswa.php

--- application/modules/Raport/views/RaportKelas/daftarSiswa.php
+++ application/modules/Raport/views/RaportKelas/daftarSiswa.php
@@ -108,7 +108,7 @@
 	    	<?php $nilaiAkhir 			= $jmlNilaiPengetahuan->nilai_pengetahuan+$jmlNilaiKeterampilan->nilai_keterampilan; ?>
 	    	<?php #$valueArray 			= "'1530,1531,1624,654,'"; ?>
 	    	<?php $valueArray 			= implode(',',$nilaiKelas); ?>
-	  	
+	  	    <?= count($nilaiKelas) ?>
            <tr data-id=>
                 <td><?= $no++ ?></td>
                 <td><?= value($list->nipd) ?></td>
