40339f6851f999aa5ff5ca4c9b8cc2073ab7cda4
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-09 14:49:17 +0700

    Update daftarSiswa.php

--- application/modules/Raport/views/RaportKelas/daftarSiswa.php
+++ application/modules/Raport/views/RaportKelas/daftarSiswa.php
@@ -95,6 +95,7 @@
 	    	<?php $sumNilaiPengetahuan = $data->nilai_pengetahuan ?>
 	    	<?php $sumNilaiKeterampilan = $data->nilai_keterampilan ?>
 	    	<?php $nilaiKelas[] = $sumNilaiPengetahuan+$sumNilaiKeterampilan ?>
+            <?php $arrayPengetahuan[] = $sumNilaiPengetahuan ?>
 		<?php endforeach ?>
     	<?php $no = 1; ?>
     	<?php foreach ($dataSiswa as $list): ?>
@@ -107,7 +108,7 @@
             <?php $nilaiPrilaku         = $this->Raport_m->nilaiPrilaku($idMasterLeger, $list->NIK_pd); ?>
 	    	<?php $nilaiAkhir 			= $jmlNilaiPengetahuan->nilai_pengetahuan+$jmlNilaiKeterampilan->nilai_keterampilan; ?>
 	    	<?php #$valueArray 			= "'1530,1531,1624,654,'"; ?>
-	    	<?php $valueArray 			= implode(',',$nilaiKelas); ?>
+	    	<?php $valueArray 			= implode(',',$arrayPengetahuan.$nilaiKelas); ?>
 
            <tr data-id=>
                 <td><?= $no++ ?></td>
