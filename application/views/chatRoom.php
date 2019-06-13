fb438f960c28d15a3b13486833810e046ca715ac
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-13 11:01:08 +0700

    Update chatRoom.php

--- application/views/chatRoom.php
+++ application/views/chatRoom.php
@@ -1,2 +1,12 @@
+<div id="preloader" class="m-page--loading-enabled m-page--loading">
+    <div class="m-page-loader m-page-loader--base">
+        <div class="m-blockui">
+            <span>Memuat Halaman ChatRoom. . .</span>
+            <span>
+                <div class="m-loader m-loader--brand"></div>
+            </span>
+        </div>
+    </div>
+</div>
 <div id="chatroom"></div>
 <script src="https://chatwee-api.com/v2/script/5cf401b1bd616d6b5d1c2c5e.js"></script>
