5684a1d6cd51aee27873ea822374e30484d825f6
iqbalrevvin <iqbal.revvin@gmail.com>
2019-06-13 15:48:02 +0700

    Create CetakLeger.php

---
+++ application/modules/Raport/controllers/CetakLeger.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CetakLeger extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		//Do your magic here
+	}
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file CetakLeger.php */
+/* Location: ./application/modules/Raport/controllers/CetakLeger.php */
